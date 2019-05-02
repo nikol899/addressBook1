ad53380f616bddf21d268b4af022727ad379a009
nicole andersen <nikol.899@gmail.com>
2019-05-02 08:52:47 -0400

    addressUpdate

--- script.js
+++ script.js
@@ -17,6 +17,7 @@ class AddressBook {
         ]; // array of Contact instance
     }
     add(info) { //makes a new contact instance add to addressbook
+       
         this.contacts.push(info);
     }
 
@@ -31,11 +32,21 @@ class AddressBook {
 }
 const book = new AddressBook(); // created new instance
 console.log(book);
-
 book.add(new Contact("Barbara", "b@yahoo.com", "111-222-3333", "Grandma"));
-
 book.deleteAt("Susan");
 book.print();
+
+let choice = prompt("Would you like to add, delete,print or quit?");
+while(true){
+    if(choice === "add"){
+        let name = prompt("Name");
+        let email= prompt("Email");
+        let phone = prompt("Phone");
+        let relation =prompt("Relation");
+        book.add(new Contact(name, email,phone,relation));
+    }
+
+    }
 
 
 
